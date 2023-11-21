7522a4d8c9c423a2b20510ca8f079815b1c73d40
Aitor Reviriego Amor <aitor.reviriego@leanmind.es>
2023-11-21 22:00:27 +0100

    :white_check_mark: WordWrapShould the same word when the columns are greater than the word length

--- src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
+++ src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
@@ -1,21 +1,9 @@
 package dev.kata.wordWrap
 
-class StringCalculator(
-    private val dependency: StringCalculatorDependency,
-) {
-    fun execute() {
-        dependency.doSomething()
-    }
-}
-
-class StringCalculatorDependency {
-    fun doSomething() {}
-}
-
 class WordWrap {
     companion object {
         fun wrap(word: String, columns: Int): String {
-            TODO("Not yet implemented")
+            return word
         }
     }
 
