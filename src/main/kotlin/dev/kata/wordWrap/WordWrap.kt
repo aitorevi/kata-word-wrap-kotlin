e74a12f0a73e6046a9316b27408b6845eddbbf44
Aitor Reviriego Amor <aitor.reviriego@leanmind.es>
2023-11-21 22:15:50 +0100

    :white_check_marc: WordWrapShould break the word by the column width

--- src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
+++ src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
@@ -3,6 +3,10 @@ package dev.kata.wordWrap
 class WordWrap {
     companion object {
         fun wrap(word: String, columns: Int): String {
+            if (word.length > columns) {
+                val wordWithLineBreack = word.substring(0,2).plus("\n").plus(word.substring(2))
+                return wordWithLineBreack
+            }
             return word
         }
     }
