58c10e62709d08f74191427cfd40c04534a7c564
Aitor Reviriego Amor <aitor.reviriego@leanmind.es>
2023-11-21 22:32:17 +0100

    :recycle: WordWrapShould add the line break by the column width in the word in another position

--- src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
+++ src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
@@ -3,12 +3,16 @@ package dev.kata.wordWrap
 class WordWrap {
     companion object {
         fun wrap(word: String, columns: Int): String {
-            if (word.length > columns) {
-                val wordWithLineBreack = word.substring(0,columns).plus("\n").plus(word.substring(columns))
+            val wordLongerThanTheColumnWidth = word.length > columns
+            if (wordLongerThanTheColumnWidth) {
+                val wordWithLineBreack = addLineBreacks(word, columns)
                 return wordWithLineBreack
             }
             return word
         }
+
+        private fun addLineBreacks(word: String, columns: Int) =
+            word.substring(0, columns).plus("\n").plus(word.substring(columns))
     }
 
 }
