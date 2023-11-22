5affc659ee52f7b6e205c5aa1369e144e68625d7
Aitor Reviriego Amor <aitor.reviriego@leanmind.es>
2023-11-22 10:56:59 +0100

    :white_check_mark: WordWrapShould add the line break by the column width in the space and words

--- src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
+++ src/main/kotlin/dev/kata/wordWrap/WordWrap.kt
@@ -4,21 +4,34 @@ class WordWrap {
     companion object {
         fun wrap(word: String, columns: Int): String {
             val wordLongerThanTheColumnWidth = word.length > columns
-            if (wordLongerThanTheColumnWidth) {
-                return addLineBreacks(word, columns)
+            if (wordLongerThanTheColumnWidth || word.isNotEmpty()) {
+                return addLineBreaks(word, columns)
             }
             return word
         }
 
-        private fun addLineBreacks(word: String, columns: Int):String {
-            if(word.contains(" ")) {
-                val replaceSpacesByLineBreaks = word.replace(" ", "\n")
-                return replaceSpacesByLineBreaks
+        private fun addLineBreaks(word: String, columns: Int): String {
+            var wordWrapped = ""
+            var restOfWord = word
+
+            val wordIsNotEmpty = restOfWord.isNotEmpty()
+            while (wordIsNotEmpty) {
+                if (restOfWord.length <= columns) {
+                    wordWrapped = wordWrapped.plus(restOfWord)
+                    restOfWord = ""
+                } else {
+                    val lastSpaceInColumWidth = restOfWord.lastIndexOf(' ', columns)
+                    var positionWrap = lastSpaceInColumWidth
+
+                    val notSpacesInColumnWidth = positionWrap == -1
+                    if (notSpacesInColumnWidth) {
+                        positionWrap = columns
+                    }
+                    wordWrapped = wordWrapped.plus(restOfWord.substring(0, positionWrap).plus("\n"))
+                    restOfWord = restOfWord.substring(positionWrap).trim()
+                }
             }
-            val addLineBreackInWord = word.substring(0, columns).plus("\n").plus(word.substring(columns))
-            return addLineBreackInWord
+            return wordWrapped
         }
-
     }
-
 }
